270cb0cf338ddfd3e67833b176604be1ca2128a9
LatteCappuchino <Honorx8user@gmail.com>
2024-10-10 13:24:11 +0300

    Create ClickDetector.lua

---
+++ ClickDetector.lua
@@ -0,0 +1,37 @@
+--[[In this tutorial,
+you will learn how to fire a ClickDetector with a..
+Script!
+Let's go!
+]]
+
+local cd=game.Workspace.Part.ClickDetector
+
+function fire()
+fireclickdetector(cd)
+end
+wait(1)
+fire()
+
+--[[
+local cd=game.Workspace.Part.ClickDetector
+This variable gets the ClickDetector that we will fire.
+
+function fire()
+Starts the function.
+
+fireclickdetector(cd)
+This stroke of code will fire the chosen ClickDetector.
+
+end
+Ends the function.
+
+wait(1)
+Waits 1 Second after executing the script, before running the next code.
+
+fire()
+Fires the function, which fires the chosen ClickDetector.
+]]
+--[[QUICK REMINDER!!
+You can copy&paste this code,
+the text wont break the code.
+]]
